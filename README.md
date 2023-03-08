ce9af3cd045e5bd34fac14a2ff0da3c817c28aa6
Louis Ouellet <louis_ouellet@hotmail.com>
2023-03-07 21:19:23 -0500

    2023-03-07 21:19 - v1.0.1

--- README.md
+++ README.md
@@ -6,11 +6,17 @@
 ![GitHub top language](https://img.shields.io/github/languages/top/LouisOuellet/php-logger?style=for-the-badge)
 ![Version](https://img.shields.io/github/v/release/LouisOuellet/php-logger?label=Version&style=for-the-badge)
 
-## Features
-  - Easy to use logging library
+## Description
+The `phpLogger` class is a PHP package for logging messages to files. It supports logging at different levels of severity, including DEBUG, INFO, SUCCESS, WARNING, and ERROR. It also provides support for rotating log files, which can help manage file sizes and ensure that logs don't become too large.
 
-## Why you might need it
-If you are looking for an easy way to integrate logging in your project. This PHP Class is for you.
+## Features
+  - Supports logging at different severity levels: DEBUG, INFO, SUCCESS, WARNING, and ERROR.
+  - Rotates log files to manage file sizes.
+  - Provides the ability to add, set, and list log files.
+  - Supports logging to multiple files.
+
+## Why you might need it?
+Logging is an essential part of debugging and monitoring software applications. It can help developers identify issues and bugs, as well as provide insight into how users are interacting with the software. The `phpLogger` class provides a simple and flexible way to log messages to files in PHP applications. It can help developers quickly set up logging functionality and manage log files, making it an essential tool for any PHP project.
 
 ## Can I use this?
 Sure!
@@ -19,7 +25,7 @@ Sure!
 This software is distributed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.en.html) license. Please read [LICENSE](LICENSE) for information on the software availability and distribution.
 
 ## Requirements
-* PHP >= 5.6.0
+* PHP >= 8.2
 
 ## Security
 Please disclose any vulnerabilities found responsibly – report security issues to the maintainers privately.
@@ -31,10 +37,10 @@ composer require laswitchtech/php-logger
 ```
 
 ## How do I use it?
-In this documentations, we will use a table called users for our examples.
-
-### Example
+### Usage
 #### Initiate phpLogger
+To use `phpLogger`, simply include the phpLogger.php file and create a new instance of the `phpLogger` class. By default, it will create a log file named "default.log" in the same directory as the phpLogger.php file.
+
 ```php
 
 //Import phpLogger class into the global namespace
@@ -46,4 +52,85 @@ require 'vendor/autoload.php';
 
 //Initiate phpLogger
 $phpLogger = new phpLogger();
+```
+
+#### Logging Messages
+To log a message, use the log method of the `phpLogger` class. By default, it will log the message at the INFO level to the "default.log" file.
+
+```php
+$phpLogger->log("This is a log message");
+```
+
+You can also specify a log level and log file name:
+
+```php
+$phpLogger->log("This is a debug message", phpLogger::LEVEL_DEBUG, "debug");
+```
+
+#### Customizing Log Files
+You can add, switch between, and rotate log files using the add, set, and rotate methods of the `phpLogger` class.
+
+```php
+// Add a new log file named "error.log"
+$phpLogger->add("error", "error.log");
+
+// Switch to the "error" log file
+$phpLogger->set("error");
+
+// Rotate the current log file
+$phpLogger->rotate();
+```
+
+#### List Log Files
+You can also list all of the log files using the list method:
+
+```php
+$files = $phpLogger->list();
+print_r($files);
+```
+
+#### Customizing Log Message Format
+You can customize the format of the logged message by subclassing `phpLogger` and overriding the log method. For example, the following code logs the message with the date and time, log level, calling function or method, file name, and line number:
+
+```php
+class MyLogger extends phpLogger {
+  public function log($message, $level = self::LEVEL_INFO, $logName = null){
+    $trace = debug_backtrace();
+    $caller = isset($trace[1]) ? $trace[1] : $trace[0];
+    $file = isset($caller['file']) ? $caller['file'] : '';
+    $line = isset($caller['line']) ? $caller['line'] : '';
+    $class = isset($caller['class']) && count($trace) > 1 ? $caller['class'] : '';
+    $function = isset($caller['function']) && count($trace) > 1 ? $caller['function'] : '';
+
+    $classTrace = '';
+    if($class != ''){
+      $classTrace .= $class . '::';
+    }
+    if($function != ''){
+      $classTrace .= $function;
+    }
+    if($classTrace != ''){
+      $classTrace = " [$classTrace]";
+    }
+
+    $timestamp = date("Y-m-d H:i:s");
+    $logLine = "[$timestamp] [$level]$classTrace ($file:$line) $message"
+
+    if(is_file($logFile)){
+      $today = new DateTime();
+      $logDate = new DateTime();
+      $logDate->setTimestamp(filemtime($logFile));
+      if($today->format("Y-m-d") > $logDate->format("Y-m-d")){
+        $fileName = $logFile . '.' . strtotime($logDate->format("Y-m-d"));
+        rename($logFile, $fileName);
+      }
+    }
+
+    file_put_contents($logFile, $logLine, FILE_APPEND);
+    
+    if(defined('STDIN')){
+      echo $logLine;
+    }
+  }
+}
 ```
--- src/phpLogger.php
+++ src/phpLogger.php
@@ -3,8 +3,9 @@
 //Declaring namespace
 namespace LaswitchTech\phpLogger;
 
-// Import some php Classes
+// Importing Dependencies
 use DateTime;
+use Exception;
 
 class phpLogger {
 
@@ -14,84 +15,139 @@ class phpLogger {
   const LEVEL_WARNING = 'WARNING';
   const LEVEL_ERROR = 'ERROR';
 
-  private $logFiles = [];
-  private $logFile = 'default';
-  private $logRotation = true;
+  private $logFiles = []; // An array to hold all added log files
+  private $logFile = 'default'; // The name of the current log file to write logs to
+  private $logRotation = true; // Whether log file rotation is enabled or not
 
+  /**
+   * Create a new logger instance.
+   *
+   * @param  string|array|null  $logFile
+   * @return void
+   * @throws Exception
+   */
   public function __construct($logFile = null){
     if($logFile != null){
       if(is_string($logFile)){
+        // If $logFile is a string, add it as a default log file with the name 'default'
         $this->add($this->logFile, $logFile);
       } else {
         if(is_array($logFile)){
+          // If $logFile is an array, add each key-value pair as a log file
           foreach($logFile as $name => $file){
             $this->add($name, $file);
           }
+          // Set the current log file to the first added log file
           $this->logFile = array_key_first($this->logFiles);
         } else {
-          throw new Exception("Could not configure phpLogger");
+          // If $logFile is neither a string nor an array, throw an exception
+          throw new Exception("Could not configure phpLogger. Invalid argument.");
         }
       }
     } else {
+      // If $logFile is null, add a default log file with the name 'default' and the filename 'default.log'
       $this->add($this->logFile, $this->logFile . '.log');
     }
   }
 
+  /**
+   * Enable or disable log file rotation.
+   *
+   * @param  bool  $bool
+   * @return void
+   * @throws Exception
+   */
   public function rotate($bool = true){
     if(is_bool($bool)){
       $this->logRotation = $bool;
     } else{
-      throw new Exception("Argument must be boolean");
+      throw new Exception("Argument must be boolean.");
     }
   }
 
+  /**
+   * Add a new log file.
+   *
+   * @param  string  $logName
+   * @param  string  $logFile
+   * @return void
+   * @throws Exception
+   */
   public function add($logName, $logFile){
     if(is_string($logName) && is_string($logFile)){
       if(!isset($this->logFiles[$logName])){
         $this->logFiles[$logName] = $logFile;
       } else {
-        throw new Exception("This log file already exist");
+        throw new Exception("This log file already exist.");
       }
     } else {
-      throw new Exception("Both arguments must be strings");
+      throw new Exception("Both arguments must be strings.");
     }
   }
 
+  /**
+   * Set the current log file.
+   *
+   * @param  string  $logName
+   * @return void
+   * @throws Exception
+   */
   public function set($logName){
     if(is_string($logName)){
       if(isset($this->logFiles[$logName])){
         $this->logFile = $logName;
       } else {
-        throw new Exception("Could not find the requested log");
+        throw new Exception("Could not find the requested log.");
       }
     } else {
-      throw new Exception("Argument must be string");
+      throw new Exception("Argument must be string.");
     }
   }
 
+  /**
+   * Get the name of the current log file.
+   *
+   * @return string
+   */
   public function get(){
     return $this->logFile;
   }
 
+  /**
+   * Get an array of all added log files.
+   *
+   * @return array
+   */
   public function list(){
     return $this->logFiles;
   }
 
+  /**
+   * Write a log message to the current log file.
+   *
+   * @param  mixed  $message
+   * @param  string  $level
+   * @param  string|null  $logName
+   * @return void
+   */
   public function log($message, $level = self::LEVEL_INFO, $logName = null){
 
     // Validate log level
     if(!in_array($level,['DEBUG','INFO','SUCCESS','WARNING','ERROR'])){
+      // If the specified log level is invalid, set it to 'DEBUG'
       $level = self::LEVEL_DEBUG;
     }
 
     // Sanitize message
     if(!is_string($message)){
+      // If the message is not a string, encode it as a JSON string and prepend '[JSON]'
       $message = '[JSON] ' . PHP_EOL . json_encode($message, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
     }
 
     // Identify the log file
     if($logName == null || !isset($this->logFiles[$logName])){
-      $logName = 'default';
+      // If the log name is not specified or is not found in the added log files, use the default log file
+      $logName = $this->logFile;
     }
     $logFile = $this->logFiles[$logName];
 
@@ -103,11 +159,23 @@ class phpLogger {
     $class = isset($caller['class']) && count($trace) > 1 ? $caller['class'] : '';
     $function = isset($caller['function']) && count($trace) > 1 ? $caller['function'] : '';
 
+    // Validate trace
+    $classTrace = '';
+    if($class != ''){
+      $classTrace .= $class . '::';
+    }
+    if($function != ''){
+      $classTrace .= $function;
+    }
+    if($classTrace != ''){
+      $classTrace = " [$classTrace]";
+    }
+
     // Timestamp
     $timestamp = date("Y-m-d H:i:s");
 
     // Format Line
-    $logLine = "[$timestamp] [$level] [$class::$function] ($file:$line) $message" . PHP_EOL;
+    $logLine = "[$timestamp] [$level]$classTrace ($file:$line) $message" . PHP_EOL;
 
     // Check if logFile should be rotated
     if(is_file($logFile)){
